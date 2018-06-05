dccae82baf548955ef2bd1d2d412ccfd74249841
Soares Chen <soares.chen@gmail.com>
2018-06-05 21:25:57 +0800

    Add explanation for dict casting bug

--- demo/src/Run/Fail01.hs
+++ demo/src/Run/Fail01.hs
@@ -54,3 +54,51 @@ env = Env { foo = "foo", bar = "bar" }
 -- Correct: "((foo: foo) (bar: bar) (baz: default-baz))"
 -- Wrong: "((foo: default-foo) (bar: default-foo) (baz: default-baz))"
 defaultResult = callHandler fooBarBazHandler combinedDict env
+
+
+-- As a demonstration, consider merging the two dictionaries
+-- with the same parameters but with different orders and values.
+-- Since dict1 appears on the left, it should have overridden
+-- all values of dict2.
+
+dict1 :: Dict (?a :: String, ?b :: String, ?c :: String)
+dict1 = let ?a = "a1"; ?b = "b1"; ?c = "c1" in Dict
+
+dict2 :: Dict (?c :: String, ?b :: String, ?a :: String)
+dict2 = let ?a = "a2"; ?b = "b2"; ?c = "c2" in Dict
+
+dict3 = dict1 &-& dict2
+
+showDict :: Dict (?a :: String, ?b :: String, ?c :: String) -> String
+showDict Dict = ?a ++ " " ++ ?b ++ " " ++ ?c
+
+-- castResult1 = "a1 b1 c1"
+castResult1 = showDict $ ((dict3 <-> Dict)
+  :: Dict (?a :: String, ?b :: String, ?c :: String)) <-> Dict
+
+-- However when we cast the merged dict3 into the target dict
+-- with exact same order as dict2, it returns dict2 instead.
+
+-- castResult2 = "a2 b2 c2"
+castResult2 = showDict $ ((dict3 <-> Dict)
+  :: Dict (?c :: String, ?b :: String, ?a :: String)) <-> Dict
+
+-- All the other casting show only values from dict1, which
+-- should be the expected behavior.
+
+-- castResult3 = "a1 b1 c1"
+castResult3 = showDict $ ((dict3 <-> Dict)
+  :: Dict (?c :: String, ?a :: String, ?b :: String)) <-> Dict
+
+-- castResult4 = "a1 b1 c1"
+castResult4 = showDict $ ((dict3 <-> Dict)
+  :: Dict (?b :: String, ?a :: String, ?c :: String)) <-> Dict
+
+-- Note that when using implicit parameters as regular constraints,
+-- the bug doesn't show up and we always get values of dict1.
+showDict2 :: (?c :: String, ?b :: String, ?a :: String) => String
+showDict2 = ?a ++ " " ++ ?b ++ " " ++ ?c
+
+-- castResult5 = "a1 b1 c1"
+castResult5 = case dict3 of
+  Dict -> showDict2
